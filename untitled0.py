9bf6c37ed6adbcd8f8c9f9554b4df1a7ee2ae1d2
quynhnhu12345678910 <135814909+quynhnhu12345678910@users.noreply.github.com>
2023-06-27 10:38:42 +0700

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -4,82 +4,33 @@ from dash import dcc as dcc
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
-from flask import Flask
 
 #getting the dataset
 
-data1 = pd.read_csv('https://raw.githubusercontent.com/quynhnhu12345678910/Python/main/Copy-of-adidas.csv')
+data1 = pd.read_csv('C:/Users/huyho/Documents/code/Copy-of-adidas.csv')
 
 #dash app
 
-app = Flask(__name__)
+app = dash.Dash(__name__)
 
 #layout
 app.layout = html.Div(children = [
-    html.Div([
-        html.H1(children = 'Financial Analysis of Adidas',
-                style={'text-align': 'center'})
-    ]),
-    html.Div([
-        html.Div([
-            dcc.Dropdown(
-                id = 'geo-dropdown',
-                options = [{'label': i, 'value': i}
-                           for i in data1['Product'].unique()],
-                value = "Men's Street Footwear"
-            ),
-            dcc.Graph(id = 'price-graph')
-        ], className='six columns'),
-        html.Div([
-            dcc.Graph(id = 'bar-chart')
-        ], className='six columns')
-    ], className='row'),
-    html.Div([
-        html.Div([
-            html.Label('Select a category:'),
-            dcc.Dropdown(
-                id = 'my_dropdown',
-                options = [{'label': 'Retailers', 'value': 'Retailer'},
-                           {'label': 'Sales Method', 'value': 'Sales Method'}
-                          ],
-                value = 'Retailer',
-                multi = False,
-                clearable = False,
-            ),
-            dcc.Graph(id = 'the_graph')
-        ])
-    ], className='row')
+	html.H1(children = 'The correlation between operating profit and six distinct types of products sold'), dcc.Dropdown(id = 'geo-dropdown',
+		options =[{'label': i, 'value' : i}
+				for i in data1['Product'].unique()],
+		value = "Men's Street Footwear"),
+	dcc.Graph(id = 'price-graph')
 ])
 
 @app.callback(
-    Output(component_id = 'price-graph', component_property ='figure'),
-    Input(component_id = 'geo-dropdown', component_property = 'value')
+	Output(component_id = 'price-graph',component_property ='figure'),
+	Input(component_id = 'geo-dropdown',component_property = 'value')
 )
-def update_scatter(selected_product):
-    a = data1[data1['Product'] == selected_product]
-    graph = px.scatter(a, x ='Units Sold', y = 'Operating Profit', color = 'Sales Method')
-    return graph
 
-@app.callback(
-    Output(component_id = 'bar-chart', component_property ='figure'),
-    Input(component_id = 'geo-dropdown', component_property = 'value')
-)
-def update_bar(selected_product):
-    b = data1[data1['Product'] == selected_product]
-    bar = px.histogram(b, x ='Sales Method', histfunc='count')
-    return bar
-
-
-@app.callback(
-    Output(component_id = 'the_graph', component_property = 'figure'),
-    Input(component_id = 'my_dropdown', component_property = 'value')
-)
-def update_pie(selected_column):
-    if selected_column == 'Retailer':
-        piechart = px.pie(data_frame = data1, names = 'Retailer', hole = 0.3)
-    elif selected_column == 'Sales Method':
-        piechart = px.pie(data_frame = data1, names = 'Sales Method', hole = 0.3)
-    return piechart
+def update_graph(selected_product):
+	a = data1[data1['Product'] == selected_product]
+	graph = px.scatter(a, x ='Units Sold' , y = 'Operating Profit',trendline="lowess",color ="Sales Method")
+	return graph 
 
 if __name__ == '__main__':
-    app.run()
+	app.run_server(debug = True)
